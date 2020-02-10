da623cd0787f343a06be1bcbb557f4026aef5bca
Bruno <b.manchinelli@gmail.com>
2020-02-10 19:53:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/buttonclickcounter/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.buttonclickcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private TextView textView;
+    private static final String TAG = "MainActivity";
+    private final String TEXT_CONTENTS = "TextContents";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        userInput.setText("");
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = userInput.getText().toString();
+                result = result + "\n";
+                textView.append(result);
+                userInput.setText("");
+
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
+        Log.d(TAG, "onCreate: out");
+
+    }
+
+    
+}
