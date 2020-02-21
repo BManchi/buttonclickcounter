8b21434263015d1c26ed6f6306d7f2f16049945b
Bruno <b.manchinelli@gmail.com>
2020-02-20 21:40:42 -0300

    Resources

--- app/src/main/java/com/example/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/example/buttonclickcounter/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.buttonclickcounter;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
